d8548695fb91419562f29710da597d4dda7b592b
Jonny <jonnyal96@gmail.com>
2021-05-13 20:22:21 -0500

    Cambios en ruta para inicio de component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { ErrosComponent } from './Pages/erros/erros.component';
 import { UsereditComponent } from './Usuarios/useredit/useredit.component';
 import { UsersComponent } from './Usuarios/users/users.component';
@@ -7,6 +8,7 @@ import { UsersComponent } from './Usuarios/users/users.component';
 const routes: Routes = [
   { path: 'usuarios', component: UsersComponent },
   { path: 'usuario/:id', component: UsereditComponent },
+  { path: '', component: AppComponent },
   { path: '**', component: ErrosComponent }
 ];
 
