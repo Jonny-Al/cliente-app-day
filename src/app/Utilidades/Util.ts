4d3261ab7f48ff317c5dd4da79810697cac473a3
Jonny <jonnyal96@gmail.com>
2021-05-15 19:34:40 -0500

    Cambio en path de acceso a api localhost y toat por columna

--- src/app/Utilidades/Util.ts
+++ src/app/Utilidades/Util.ts
@@ -2,6 +2,6 @@
 export class Util {
     // Url path para consumir service rest
     static getUrl() {
-        return 'http://192.168.20.21:8080/api';
+        return 'http://localhost:8080/api';
     }
 }
