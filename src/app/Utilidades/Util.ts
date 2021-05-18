f216c437ad63a1f6824529a2fa487956453c1b16
Jonny <jonnyal96@gmail.com>
2021-05-18 16:04:37 -0500

    Cambio de puerto para uso de keycloak

--- src/app/Utilidades/Util.ts
+++ src/app/Utilidades/Util.ts
@@ -2,6 +2,6 @@
 export class Util {
     // Url path para consumir service rest
     static getUrl() {
-        return 'http://localhost:8080/api';
+        return 'http://localhost:8081/api';
     }
 }
